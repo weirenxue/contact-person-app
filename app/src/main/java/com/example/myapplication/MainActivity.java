184d0c9032c1f015ce42adf35c7fc6ae2997a89e
薛惟仁 <wrxue@hj-rfid.com>
2020-04-11 20:00:35 +0800

    Initial project.  1. Can display the contact person's data(name, phone) one MainActitvity.  2. Use thread for connecting to API and handler for UI update.

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Handler mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                switch(msg.what){
+                    case 1:
+                        try {
+                            int num = 0;
+                            LinearLayout container = (LinearLayout) findViewById(R.id.container);
+                            JSONObject jsonObject = (JSONObject)msg.obj;
+                            System.out.println("in handler "+ jsonObject.getJSONArray("records").length());
+                            num = jsonObject.getJSONArray("records").length();
+                            for (int i = 0; i < num; i++) {
+                                //新增聯絡人的按扭
+                                /*Button add_user = (Button) findViewById(R.id.add_contact);
+                                add_user.setOnClickListener(new Button.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                    }
+                                });*/
+                                //新LinearLayout,每個聯絡人一個
+                                LinearLayout contact = new LinearLayout(getApplicationContext());
+                                contact.setOrientation(LinearLayout.HORIZONTAL);
+                                contact.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                                container.addView(contact);
+
+                                //放名字的TextView
+                                TextView tv_name = new TextView(getApplicationContext());
+                                tv_name.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
+                                tv_name.setText(jsonObject.getJSONArray("records").getJSONObject(i).get("name").toString());
+                                tv_name.setTextSize(20);
+                                tv_name.setGravity(Gravity.LEFT);
+                                contact.addView(tv_name);
+
+                                //放手機的TextView
+                                TextView tv_phone = new TextView(getApplicationContext());
+                                tv_phone.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 3));
+                                tv_phone.setText(jsonObject.getJSONArray("records").getJSONObject(i).get("phone").toString());
+                                tv_phone.setTextSize(20);
+                                tv_phone.setGravity(Gravity.CENTER);
+                                contact.addView(tv_phone);
+
+                                //更新的按扭
+                                Button bn_update = new Button(getApplicationContext());
+                                bn_update.setText(R.string.button_update);
+                                //bn_update.setOnClickListener(getApplicationContext());
+                                contact.addView(bn_update);
+
+                                //刪除的按扭
+                                Button bn_delete = new Button(getApplicationContext());
+                                bn_delete.setText(R.string.button_delete);
+                                bn_delete.setGravity(Gravity.CENTER);
+                                contact.addView(bn_delete);
+                                //bn_delete.setOnClickListener(getApplicationContext());
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+            }
+        };
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                Message msg = new Message();
+                PostgresqlAPI pgapi = new PostgresqlAPI();
+                int num = 0;
+                JSONObject jsonObject = null;
+
+                try {
+                    jsonObject = pgapi.queryUser();
+                    System.out.println("The number of users：" + jsonObject.getJSONArray("records").length());
+                    msg.obj = (Object)jsonObject;
+                    // query
+                    msg.what = 1;
+                    mHandler.sendMessage(msg);
+                    /*num = jsonObject.getJSONArray("records").length();
+                    for (int i = 0; i < num; i++) {
+                        //新增聯絡人的按扭
+                        Button add_user = (Button) findViewById(R.id.add_contact);
+                        add_user.setOnClickListener(new Button.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                            }
+                        });
+                        //新LinearLayout,每個聯絡人一個
+                        LinearLayout contact = new LinearLayout(getApplicationContext());
+                        contact.setOrientation(LinearLayout.HORIZONTAL);
+                        contact.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        container.addView(contact);
+
+                        //放名字的TextView
+                        TextView tv_name = new TextView(getApplicationContext());
+                        tv_name.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
+                        tv_name.setText(jsonObject.getJSONArray("records").getJSONObject(i).get("name").toString());
+                        tv_name.setTextSize(20);
+                        tv_name.setGravity(Gravity.LEFT);
+                        contact.addView(tv_name);
+
+                        //放手機的TextView
+                        TextView tv_phone = new TextView(getApplicationContext());
+                        tv_phone.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 3));
+                        tv_phone.setText(jsonObject.getJSONArray("records").getJSONObject(i).get("phone").toString());
+                        tv_phone.setTextSize(20);
+                        tv_phone.setGravity(Gravity.CENTER);
+                        contact.addView(tv_phone);
+
+                        //更新的按扭
+                        Button bn_update = new Button();
+                        bn_update.setText(R.string.button_update);
+                        //bn_update.setOnClickListener(getApplicationContext());
+                        contact.addView(bn_update);
+
+                        //刪除的按扭
+                        /*Button bn_delete = new Button(getApplicationContext());
+                        bn_delete.setText(R.string.button_delete);
+                        bn_delete.setGravity(Gravity.CENTER);
+                        contact.addView(bn_delete);
+                        //bn_delete.setOnClickListener(getApplicationContext());
+                    }*/
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+    @Override
+    public void onClick(View v) {
+        ((Button) v).setText("");
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/PostgresqlAPI.java
@@ -0,0 +1,107 @@
+package com.example.myapplication;
+
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author wrxue
+ */
+public class PostgresqlAPI {
+    String apiUrl;
+    String createPhp;
+    String readPhp;
+    String updatePhp;
+    String deletePhp;
+    String readOnePhp;
+    // private Connection con;
+    /**
+     *
+     */
+    public PostgresqlAPI(){
+        apiUrl = "http://192.168.0.13/api/user/";
+        createPhp = "create.php";
+        readPhp = "read.php";
+        updatePhp = "update.php";
+        deletePhp = "delete.php";
+        readOnePhp = "read_one.php";
+    }
+    /*connect to db*/
+    public static String httpRequest(String requestUrl, String requestMethod, String outputStr){
+        StringBuffer buffer=null;
+        try{
+            //根據requestUrl創建一個HttpURLConnection物件，此時只是創物件，還沒有真的連線。
+            URL url=new URL(requestUrl);
+            HttpURLConnection conn=(HttpURLConnection)url.openConnection();
+            //需要寫JSON給API，所以DoOutput要打開，要讀API來的JSON，DoInput也要打開。
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            //設定http的Request方法，GET/POST
+            conn.setRequestMethod(requestMethod);
+            //要傳給server的Data，此時只有寫進物件，還沒有送上server。
+            if(null!=outputStr){
+                OutputStream os=conn.getOutputStream();
+                os.write(outputStr.getBytes("utf-8"));
+                os.close();
+            }
+
+            //getInputStream這函式會將上面的設定，傳送給server，InputStream以Byte為單位的輸入串流。
+            InputStream is=conn.getInputStream();
+            //InputStream, InputStreamReader, BufferedReader三者關係參考
+            //https://dotblogs.com.tw/hanktom/2016/01/26/180721
+            InputStreamReader isr=new InputStreamReader(is,"utf-8");
+            BufferedReader br=new BufferedReader(isr);
+            buffer=new StringBuffer();
+            String line=null;
+            while((line=br.readLine())!=null){
+                buffer.append(line);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+
+    public JSONObject queryUser() throws JSONException {
+        String url = apiUrl + readPhp;
+        String str = httpRequest(url, "GET", null);
+        JSONObject jsonObject = new JSONObject(str);
+        return jsonObject;
+    }
+    public JSONObject queryOneUser(int id) throws JSONException {
+        String url = apiUrl + readOnePhp + "?id=" + id;
+        String str = httpRequest(url, "GET", null);
+        JSONObject jsonObject = new JSONObject(str);
+        return jsonObject;
+    }
+    public boolean insertUser(JSONObject jsonObject) throws JSONException {
+        String url = apiUrl + createPhp;
+        String str = httpRequest(url, "POST", jsonObject.toString());
+        JSONObject response = new JSONObject(str);
+        return (boolean)response.get("success");
+    }
+    public boolean updateUser(JSONObject jsonObject) throws JSONException {
+        String url = apiUrl + updatePhp;
+        String str = httpRequest(url, "POST", jsonObject.toString());
+        JSONObject response = new JSONObject(str);
+        return (boolean)response.get("success");
+    }
+    public boolean deleteUser(int id) throws JSONException {
+        JSONObject jsonObject = new JSONObject("{\"id\":" + id + "}");
+        String url = apiUrl + deletePhp;
+        String str = httpRequest(url, "POST", jsonObject.toString());
+        JSONObject response = new JSONObject(str);
+        return (boolean)response.get("success");
+    }
+
+
+
+}
